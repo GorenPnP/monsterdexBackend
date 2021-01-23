9687e940214d7c6ed7a8e374d705ecbb64c1cd85
Vanessa Steinbrügge <v.steinbr@gmail.com>
2021-01-23 12:26:22 +0100

    generic startup (path)

--- venv/startup.sh
+++ venv/startup.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-cd ~/Schreibtisch/monsterBackend/venv
 
-source ./bin/activate
-pip3 install -r requirements.txt
-export FLASK_APP=app/app.py
-export FLASK_ENV=development
-code .
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH && \
+
+source ./bin/activate && \
+pip3 install -r requirements.txt && \
+export FLASK_APP=app/app.py && \
+export FLASK_ENV=development && \
 
 flask run
