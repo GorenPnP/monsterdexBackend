2bcfce4194b607310e4d5694f20dbb1b4937d02e
Vanessa Steinbrügge <v.steinbr@gmail.com>
2021-02-06 10:58:56 +0100

    cached images, fixed image path issue

--- venv/app/models.py
+++ venv/app/models.py
@@ -3,7 +3,7 @@ from graphene import ObjectType, String, Int, Float, List, Field, ID, Enum
 from PIL import Image
 
 from io import BytesIO
-import base64
+import base64, os
 from pathlib import Path
 
 class Efficiency(Enum):
@@ -83,14 +83,14 @@ class Monster(ObjectType):
 	def resolve_thumbnail(self, info):
 		max_size = 128, 128
 		id = self.get("id")
-
-		return _get_resized_png(Path(f"monsterdexBackend/venv/static/monster_images/{id}.png"), max_size)
+		
+		return _get_resized_png(id, max_size)
 
 	def resolve_image(self, info):
 		max_size = 1024, 1024
 		id = self.get("id")
 
-		return _get_resized_png(Path(f"monsterdexBackend/venv/static/monster_images/{id}.png"), max_size)
+		return _get_resized_png(id, max_size)
 
 	def resolve_forms(self, info):
 		query = "select mon_2 from monster_forms where mon_1 = ? ORDER BY mon_2"
@@ -143,12 +143,34 @@ class Monster(ObjectType):
 		return [dict(row) for row in result]
 
 
-def _get_resized_png(path, max_size):
+def _get_resized_png(monster_id: int, max_size: (int, int)): # max_size = (width, height) in px
 
+	# safe from injecting paths to read the filesystem
+	if type(monster_id) is not int: return ''
+
+	# need a few paths ..
+	path_of_original_images = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../static/monster_images/")
+	path_of_scaled_images = os.path.join(path_of_original_images, f"{max_size[0]}_{max_size[1]}")
+	path_of_image = os.path.join(path_of_scaled_images, f"{monster_id}.png")
+
+	# if saved of given size exists, use that
+	try:
+		image = Image.open(path_of_image, mode="r")
+
+	except FileNotFoundError:
+		print("Generating new sized image:", path_of_image)
+
+		# else: create img of that size from original
+		path_of_original = os.path.join(path_of_original_images, f"{monster_id}.png")
+
+		image = Image.open(path_of_original, mode="r")
+		image.thumbnail(max_size, resample=Image.BICUBIC)
+
+		if not os.path.exists(path_of_scaled_images):
+			os.mkdir(path_of_scaled_images)
+		image.save(path_of_image, "PNG")
+
+	# format to base64 & return it
 	buffer = BytesIO()
-
-	image = Image.open(path, mode="r")
-	image.thumbnail(max_size, resample=Image.BICUBIC)
-
 	image.save(buffer, format="PNG")
 	return base64.b64encode(buffer.getvalue()).decode("utf-8")
